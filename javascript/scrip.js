2f326db383baf4c370da5aa572e0024363aaa938
Clayton Santos Macedo <121825051+JuniorZero@users.noreply.github.com>
2023-07-27 21:42:10 -0300

    Add files via upload

---
+++ javascript/scrip.js
@@ -0,0 +1,11 @@
+/*TL12 Desenvolvimento da abertura e fechamento do menu lateral*/
+/*TL12.1 Abertuda*/
+function abrirNav(){
+    document.getElementById("menu-oculto").style.width="250px";
+    document.getElementById("").style.marginLeft="250px";
+}
+/*TL12.2 Fechamento*/
+function fecharNav(){
+    document.getElementById("menu-oculto").style.width="0px";
+    document.getElementById("").style.marginLeft="0px";    
+}
